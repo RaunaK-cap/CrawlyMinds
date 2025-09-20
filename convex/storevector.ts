8644c07ffed66e01318ebd6913a3278c1b8cb942
Raunak <raunakkumar6437@gmail.com>
2025-09-21 03:51:11 +0530

    fixed issue duplication

--- convex/storevector.ts
+++ convex/storevector.ts
@@ -6,16 +6,27 @@ export const storeUsersData = mutation({
         UserID: v.string(),
         Email: v.string(),
         Name: v.string(),
-        CreatedAt: v.number()
+        
     },
-
+    
     handler: async (ctx, args) => {
-        await ctx.db.insert("UserSchema", {
+        const existingUser = await ctx.db
+                .query("UserSchema")
+                .withIndex("by_UserId" , q => q.eq("UserID" , args.UserID))
+                .unique()
+
+        if(existingUser){
+            await ctx.db.patch(existingUser._id , {
+                Email:args.Email,
+                Name:args.Name
+            })
+            return existingUser._id
+        }
+
+        return await ctx.db.insert("UserSchema", {
             UserID: args.UserID,
             Email: args.UserID,
-            Name: args.Name,
-            CreatedAt: Date.now()
-
+            Name: args.Name,  
         })
     }
 })
@@ -46,7 +57,7 @@ export const storeEmbeddings = mutation({
             Text_Chunk: args.Text_chunks,
             Vectors: args.Vectors,
             source: args.source,
-            CreatedAt: Date.now()
+            
         })
     }
 })

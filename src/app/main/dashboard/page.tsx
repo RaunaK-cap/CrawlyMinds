be73ec05f88c2231844cbb66e5bb2c78c3f3175a
Raunak <raunakkumar6437@gmail.com>
2025-10-01 13:09:11 +0530

    client side authentication

--- src/app/main/dashboard/page.tsx
+++ src/app/main/dashboard/page.tsx
@@ -1,9 +1,32 @@
 "use client"
 import { Toaster } from "@/components/ui/sonner"
 import ChatPage from "../components/vercelchatmessage"
-
+import { authClient } from "@/lib/auth-client" 
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Page() {
+  const router = useRouter()
+  const { 
+    data: session, 
+    isPending
+} = authClient.useSession()
+
+useEffect(() => {
+  if (!isPending && !session) {
+    router.push("/")
+  }
+}, [session, isPending, router])
+
+
+if (isPending) {
+  return <div className="flex items-center justify-center min-h-screen text-5xl tracking-wider ">...</div>
+}
+
+if (!session) {
+  return null 
+}
+
 
   return (
     <div className="max-h-screen">
